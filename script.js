2afc0a2c5eb4559c55afa034fda381cbc9afb436
Conner Smith <basketballbob2003@gmail.com>
2024-08-04 14:06:23 -0400

    Added links to buttons

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ document.body.addEventListener('wheel', (event) => {
     const scrollDistance = 100; 
     window.scrollBy(direction * scrollDistance, 0); 
 });
+
+function scrollToHome() {
+    document.getElementById("home-section").scrollIntoView({behavior : "smooth"})
+}
+
+function scrollToAbout() {
+    document.getElementById("about-section").scrollIntoView({behavior : "smooth"})
+}
+
+function scrollToProjects() {
+    document.getElementById("projects-section").scrollIntoView({behavior : "smooth"})
+}
+
+function scrollToContact() {
+    document.getElementById("contact-section").scrollIntoView({behavior : "smooth"})
+}
